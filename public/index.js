8d8fd288ba32d3c202d9fe355fea7bedc693178a
DevPirateSA <kellymahlangu01@gmail.com>
2024-11-18 03:52:51 +0200

    trying

---
+++ public/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const PORT = process.env.PORT || 3001;
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Welcome to My Portfolio Page!!');
+});
+app.get('/hello', (req, res) => {
+    res.send('hello world');
+});
+app.listen(PORT, () => {
+    console.log("Listening on port " + PORT);
+});
+// module.exports = app
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -15,4 +15,4 @@ app.listen(PORT, () => {
     console.log("Listening on port " + PORT)
 })
 
-module.exports = app
+// module.exports = app
